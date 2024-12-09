1098b3e4fb5bd74fffb8cb9181e9333e6577d018
AlesR000 <alessandrorippa00@outlook.it>
2024-12-09 16:22:32 +0100

    Created the project

---
+++ Express/Express/ExpressApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpressApp.swift
+//  Express
+//
+//  Created by Alessandro Rippa on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
